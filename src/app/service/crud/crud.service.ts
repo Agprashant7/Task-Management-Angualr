6e35a1d2af8f401ab08d8295602e3b62e43ffdf7
Agprashant7 <prashantag420@gmail.com>
2023-04-20 12:37:04 +0530

    clean up

--- src/app/service/crud/crud.service.ts
+++ src/app/service/crud/crud.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Task } from '../../model/task';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment={
-    production:false,
-    title:'Task APP',
-    apiURL:'https://my-json-server.typicode.com/Agprashant7/demo-json/tasks'
-}

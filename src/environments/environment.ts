2eb6a6a7cf19760f751c4cb74aff0abd42e01c01
Agrashant7 <prashantag420@gmail.com>
2023-05-18 16:40:45 +0530

    / correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const apiHost = 'taskbackend-nskp.onrender.com';
-const authUrl = `https://${apiHost}/api`;
-const taskUrl=`https://${apiHost}/task`;
+const authUrl = `https://${apiHost}/api/`;
+const taskUrl=`https://${apiHost}/task/`;
 
 export const environment = {
   apiHost,
